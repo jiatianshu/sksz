b78c4939d93d95c90b3f555aac74e23f1fa1777f
weiguoqiang2008@qq.com <weiguoqiang123@>
2023-01-15 19:07:18 +0800

    登录页修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,7 +17,7 @@
                 <div class="login-btn">
                     <el-button type="primary" @click="submitForm()">登录</el-button>
                 </div>
-                <p class="login-tips">Tips : 用户名和密码随便填。</p>
+                
             </el-form>
         </div>
     </div>
@@ -29,8 +29,8 @@
         data: function () {
             return {
                 param: {
-                    username: 'admin',
-                    password: 'JFat0Zdc',
+                    username: '',
+                    password: '',
                 },
                 rules: {
                     username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
